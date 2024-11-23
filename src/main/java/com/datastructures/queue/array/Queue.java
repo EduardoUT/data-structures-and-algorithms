43062c023af20193992ac0d6e5277ef2a855a108
Eduardo Reyes Hernández <mcorey316@gmail.com>
2024-11-22 18:15:58 -0600

    Agregando implementación de una Queue con un arreglo

---
+++ src/main/java/com/datastructures/queue/array/Queue.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.datastructures.queue.array;
+
+/**
+ * Esta estructura de datos sigue el principio FIFO (Firs In First Out).
+ *
+ * Implementada en un arrglo para este caso.
+ *
+ * Big O de operaciones:
+ *
+ * Inserción: O(1)
+ *
+ * Eliminación: O(1)
+ *
+ * Acceso: O(1) al ser elementos indexados en un arreglo.
+ *
+ * @author eduar
+ */
+public class Queue {
+
+    private final int[] arrayQueue;
+    private int rearEnd;
+    private int frontEnd;
+
+    public Queue() {
+        arrayQueue = new int[5];
+        rearEnd = -1;
+        frontEnd = -1;
+    }
+
+    /**
+     * Inserta un nuevo elemento en la cola rearEnd.
+     *
+     * @param value
+     */
+    public void enqueue(int value) {
+        if (rearEnd + 1 >= arrayQueue.length) {
+            throw new ArrayIndexOutOfBoundsException("Sobrecarga.");
+        }
+
+        if (frontEnd == -1 && rearEnd == -1) {
+            frontEnd = rearEnd = 0;
+        } else {
+            rearEnd++;
+        }
+        arrayQueue[rearEnd] = value;
+    }
+
+    /**
+     * Elimina un elemento al inicio de la cola frontEnd.
+     *
+     * @return Valor eliminado.
+     */
+    public int dequeue() {
+        int value;
+        if (frontEnd == -1 || frontEnd > rearEnd) {
+            throw new ArrayIndexOutOfBoundsException("Sobrecarga.");
+        } else {
+            value = arrayQueue[frontEnd];
+            frontEnd++;
+        }
+        return value;
+    }
+
+    /**
+     *
+     * @return Representación String de los valores en cola.
+     */
+    public String display() {
+        StringBuilder sb = new StringBuilder();
+        if (rearEnd == -1) {
+            throw new NullPointerException("Vacía.");
+        } else {
+            for (int i = frontEnd; i <= rearEnd; i++) {
+                sb.append(arrayQueue[i]).append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/datastructures/queue/array/QueueTest.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.datastructures.queue.array;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+
+/**
+ *
+ * @author eduar
+ */
+public class QueueTest {
+
+    private final Queue queue = new Queue();
+
+    public QueueTest() {
+    }
+
+    @DisplayName("Debería encolar un nuevo elemento.")
+    @Test
+    void testQueue() {
+        queue.enqueue(888);
+        assertEquals("888 ", queue.display());
+    }
+
+    @DisplayName("Debería lanzar ArrayIndexOutOfBoundsException cuando"
+            + " exceda el límite")
+    @Test
+    void testQueueFull() {
+        ArrayIndexOutOfBoundsException e;
+        queue.enqueue(777);
+        queue.enqueue(6737);
+        queue.enqueue(2773);
+        queue.enqueue(12);
+        queue.enqueue(1);
+        e = assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> {
+            queue.enqueue(766);
+        });
+        assertEquals("Sobrecarga.", e.getMessage());
+    }
+
+    @DisplayName("Debería desencolar el elemento de frente.")
+    @Test
+    void testDequeue() {
+        queue.enqueue(1777);
+        queue.enqueue(2888);
+        assertEquals(1777, queue.dequeue());
+    }
+
+    @DisplayName("Debería lanzar ArrayIndexOutOfBoundsException cuando "
+            + "elimine elementos de más.")
+    @Test
+    void testDequeueOfAnEmptyQueue() {
+        ArrayIndexOutOfBoundsException e;
+        e = assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> {
+            queue.dequeue();
+        });
+        assertEquals("Sobrecarga.", e.getMessage());
+
+        queue.enqueue(777);
+        queue.enqueue(6737);
+        queue.enqueue(2773);
+        queue.enqueue(12);
+        queue.enqueue(1);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        e = assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> {
+            queue.dequeue();
+        });
+        assertEquals("Sobrecarga.", e.getMessage());
+    }
+}
