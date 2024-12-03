494e49c615ff9773090fda623789f89f85209c68
Eduardo Reyes Hernández <mcorey316@gmail.com>
2024-12-03 14:19:28 -0600

    Agregando métodos para recorrido de árbol en-orden, pre-orden y post-orden de forma recursiva e iterativa

--- src/main/java/com/datastructures/tree/BinaryTree.java
+++ src/main/java/com/datastructures/tree/BinaryTree.java
@@ -4,6 +4,9 @@
  */
 package com.datastructures.tree;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * This data-structor is non-lineal and consists of a set of nodes, each node
  * has three parts:
@@ -28,7 +31,7 @@ package com.datastructures.tree;
  * ----------------/---\
  *
  * --------------(2)---(5)
- * 
+ *
  * -------------/---\
  *
  * -----------(3)---(4)
@@ -66,19 +69,120 @@ public class BinaryTree {
     /**
      * Traversing a BinaryTree in pre-order style in recursive way.
      *
+     * 1. Visit the root node.
+     *
+     * 2. Traverse the left subtree in pre-order fashion.
+     *
+     * 3. Traverse the right subtree in pre-order fashion.
+     *
      * @param root The root node.
      */
-    public void preOrder(TreeNode root) {
+    public void preOrderRecursive(TreeNode root) {
         if (root != null) {
             System.out.println(root.getData());
-            preOrder(root.getLeft());
-            preOrder(root.getRight());
+            preOrderRecursive(root.getLeft());
+            preOrderRecursive(root.getRight());
+        }
+    }
+
+    public void preOrderIterative() {
+        if (root != null) {
+            Deque<TreeNode> deque = new LinkedList<>();
+            deque.addFirst(root);
+            while (!deque.isEmpty()) {
+                TreeNode temp = deque.removeFirst();
+                System.out.println(temp.getData());
+                if (temp.getLeft() != null) {
+                    deque.addFirst(temp.getLeft());
+                }
+                if (temp.getRight() != null) {
+                    deque.addFirst(temp.getRight());
+                }
+            }
+        }
+    }
+
+    /**
+     * 1. Traverse the left subree in in-order fashion.
+     *
+     * 2. Visit de root node.
+     *
+     * 3. Traverse the right subtree in in-order fashion.
+     *
+     * @param root
+     */
+    public void inOrderRecursive(TreeNode root) {
+        if (root != null) {
+            inOrderRecursive(root.getLeft());
+            System.out.print(root.getData() + " ");
+            inOrderRecursive(root.getRight());
+        }
+    }
+
+    public void inOrderIterative() {
+        if (root != null) {
+            Deque<TreeNode> deque = new LinkedList<>();
+            deque.addFirst(root);
+            TreeNode temp = root;
+            while (!deque.isEmpty() || temp != null) {
+                if (temp != null) {
+                    deque.addFirst(temp);
+                    temp = temp.getLeft();
+                } else {
+                    temp = deque.removeFirst();
+                    System.out.println(temp.getData());
+                    temp = temp.getRight();
+                }
+            }
+        }
+    }
+
+    /**
+     * 1. Traverse the left subtree in post-order fashion.
+     *
+     * 2. Traverse the right subtree in post-order fashion.
+     *
+     * 3. Visit the node.
+     *
+     * @param root
+     */
+    public void postOrderRecursive(TreeNode root) {
+        if (root != null) {
+            postOrderRecursive(root.getLeft());
+            postOrderRecursive(root.getRight());
+            System.out.println(root.getData());
+        }
+    }
+
+    //17:54
+    public void postOrderIterative() {
+        if (root != null) {
+            TreeNode current = root;
+            Deque<TreeNode> deque = new LinkedList<>();
+            while (current != null || !deque.isEmpty()) {
+                if (current != null) {
+                    deque.addFirst(current);
+                    current = current.getLeft();
+                } else {
+                    TreeNode temp = deque.peekFirst().getRight();
+                    if (temp == null) {
+                        temp = deque.removeFirst();
+                        System.out.println(temp.getData() + " ");
+                        while (!deque.isEmpty() && temp == deque.peekFirst().getRight()) {
+                            temp = deque.removeFirst();
+                            System.out.println(temp.getData() + " ");
+                        }
+                    } else {
+                        current = temp;
+                    }
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
         binaryTree.createBinaryTree();
-        binaryTree.preOrder(binaryTree.getRoot());
+        binaryTree.postOrderIterative();
     }
 }
